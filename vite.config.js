01666bb8978636610f661401e4bcd9d7a96bc7a9
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-20 23:05:54 +0300

    added

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,31 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest: {
-        name: "Prayer Guide",
+        theme_color: "#be630e",
+        background_color: "#f69435",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "prayer",
         short_name: "PG",
-        description: "Prayer Guide description",
-        theme_color: "#f69435",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
